8d51f892e412177cb918096b48cd7fac482d10cc
Joseph Hunt <josephhunt@Josephs-MBP.attlocal.net>
2017-12-10 10:07:02 -0600

    remove summon tarfiles in authn/build dir

--- authn_k8s_scale_demo/test.sh
+++
@@ -1,4 +0,0 @@
-set -x
-oc project conjur
-service_desc=$(oc describe service conjur-master)
-echo $service_desc | awk '/NodePort:/ {print $2 " " $3}' | awk '/https/ {print $2}' | awk -F "/" '{ print $1 }'
