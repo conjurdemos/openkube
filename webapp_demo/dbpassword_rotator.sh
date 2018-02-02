3e45529e52bb099057fd80e07340f2a98840915e
Kevin Gilpin <kgilpin@gmail.com>
2018-02-02 10:48:58 -0500

    fix db password rotator

--- webapp_demo/dbpassword_rotator.sh
+++ webapp_demo/dbpassword_rotator.sh
@@ -1,14 +1,16 @@
-#!/bin/bash 
+#!/bin/bash -e
 set -o pipefail
-conjur authn logout
+
+source $DEMO_ROOT/$DEMO_CONFIG_FILE
+
+conjur authn logout >> /dev/null
 conjur authn login
-while [[ 1 == 1 ]]; do
-	new_pwd=$(openssl rand -hex 12)
-	error_msg=$(conjur variable values add db/password $new_pwd 2>&1 >/dev/null)
-	if [[ "$error_msg" = "" ]]; then
-		echo $(date +%X) "New db password is:" $new_pwd
-	else
-		echo $error_msg
-	fi
-	sleep 5
-done
+
+
+new_pwd=$(openssl rand -hex 12)
+error_msg=$(conjur variable values add db/password $new_pwd 2>&1 >/dev/null)
+if [[ "$error_msg" = "" ]]; then
+	echo $(date +%X) "New db password is:" $new_pwd
+else
+	echo $error_msg
+fi
